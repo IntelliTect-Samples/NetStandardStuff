5329d6d42e39b28367fee0fb15c04215680f8272
Mark Michaelis <mark@IntelliTect.com>
2017-04-19 21:48:37 -0700

    Created initial projects

---
+++ StandardLibrary/Thing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StandardLibrary
+{
+    public class Thing
+    {
+        public static string Method()
+        {
+            return nameof(StandardLibrary);
+        }
+
+        public static System.Reflection.Assembly GetExecutingAssembly()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly
+        }
+    }
+}
---
+++ StandardLibrary14/Thing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StandardLibrary14
+{
+    public class Thing
+    {
+        public static string Method()
+        {
+            return nameof(StandardLibrary14);
+        }
+
+
+    }
+}
