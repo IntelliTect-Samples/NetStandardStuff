30a7661794cce235c237c8f13e7ba223f6468c33
Mark Michaelis <mark@IntelliTect.com>
2017-04-19 22:16:16 -0700

    Removed StandardLibrary14 which was created using Visual Studio.

--- StandardLibrary/Thing.cs
+++ StandardLibrary/Thing.cs
@@ -9,9 +9,18 @@ namespace StandardLibrary
             return nameof(StandardLibrary);
         }
 
+#if Fail
+
         public static System.Reflection.Assembly GetExecutingAssembly()
         {
-            return System.Reflection.Assembly.GetExecutingAssembly
+            // Bogus IntelliSence because the incorrect project type is specified in the solution:
+            //      Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}")
+            // when adding a project created using dotnet new classlib --name StandardLibrary
+            // Changing the project Guid to
+            //      Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}")
+            // resolves the issue.
+            return System.Reflection.Assembly.GetExecutingAssembly();
         }
+#endif // Fail
     }
 }
